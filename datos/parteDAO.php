bee3da751fe12c3a7bd7df7483f29b348b3b2fc8
xabertum <mohatar@gmail.com>
2018-10-08 19:08:50 +0200

    parteDAO completed

--- datos/parteDAO.php
+++ datos/parteDAO.php
@@ -11,7 +11,39 @@ class AltaNuevoParte extends Conexion
         self::$conexion = Conexion::conectar();
     }
 
-    public static function altaParte () {
+    public static function altaParte($parte)
+    {
+        $query = "INSERT INTO Partes (DNI, Fecha_accidente, Hora_accidente, Causa_accidente,
+                Tipo_lesion, Partes_cuerpo_lesionado, Gravedad, Baja) VALUES (:DNI, :fecha,
+                :hora, :causa, :tipo, :zona, :gravedad, :baja)";
+
+        self::getConexion();
+
+        $resultado = self::$conexion->prepare($query);
+
+        $_DNI = $parte->getDNI_trabajador();
+        $_fecha = $parte->getFecha_accidente();
+        $_hora = $parte->getHora_accidente();
+        $_causa = $parte->getCausa_accidente();
+        $_tipo = $parte->getTipo_lesion();
+        $_zona = $parte->getZona_lesion();
+        $_gravedad = $parte->getGravedad();
+        $_baja = $parte->getBaja();
+
+        $resultado->bindParam(":DNI", $_DNI);
+        $resultado->bindParam(":fecha", $_fecha);
+        $resultado->bindParam(":hora", $_hora);
+        $resultado->bindParam(":causa", $_causa);
+        $resultado->bindParam(":tipo", $_tipo);
+        $resultado->bindParam(":zona", $_zona);
+        $resultado->bindParam(":gravedad", $_gravedad);
+        $resultado->bindParam(":baja", $_baja);
+
+        if ($resultado->execute()) {
+            return true;
+        } else {
+            return false;
+        }
 
     }
 
