6d8c03da3529e93f6d3edba258e5ede8383a7e63
xabertum <mohatar@gmail.com>
2018-10-30 11:45:32 +0100

    Added fules

---
+++ consulta_partes_login.php
@@ -0,0 +1,41 @@
+<?php
+include '/controlador/UsuarioControlador.php';
+include '/helps/helps.php';
+
+session_start();
+
+header('Content-type: application/json');
+
+$resultado = array();
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    
+    if (isset($_POST["usuario"]) && isset($_POST["passwd"])) {
+        
+        $txtUsuario = validar_campo($_POST['usuario']);
+        $txtPassword = validar_campo($_POST['passwd']);
+        
+        $resultado = array(
+                "estado" => "true"
+        );
+        
+        if (UsuarioControlador::login($txtUsuario, $txtPassword)) {
+            
+            $usuario = UsuarioControlador::getUser($txtUsuario, $txtPassword);
+            $_SESSION["usuario"] = array(
+                    "id" => $usuario->getId(),
+                    "nombre" => $usuario->getNombre(),
+                    "rol" => $usuario->getRol(),
+                    "username" => $usuario->getUsername(),            
+            );
+            return print(json_encode($resultado));
+            
+        }
+    }
+}
+
+$resultado = array(
+        "estado" => "false"
+);
+return print(json_encode($resultado)); 
+
